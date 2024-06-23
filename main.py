4245624e614beb6a760e6935057a8b2d1fcf8f41
Jhoan1229 <yoam45@gmail.com>
2024-06-23 12:14:22 -0400

    primer commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Proyecto Python y Mysql:
+- Abrir asistente
+- Login o registro
+- Si elegimos registro, creara un usuario en la bbdd
+- Si elegimos login, identifica al usuario y nos preguntara
+- Crear nota, mostrar notas, borrarlas.
+"""
+
+from usuarios import acciones
+
+print("""
+Acciones disponibles:
+    - registro
+    - login      
+""")
+
+hazEL = acciones.Acciones()
+accion = input("Que quieres hacer?: ")
+
+if accion == "registro":
+    hazEL.registro()
+    
+elif accion == "login":
+    hazEL.login()
